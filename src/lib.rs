8709c10ec2bb8e5fa2b576a8c57ac1ecc4d6ddbe
gotuzzoj23 <gotuzzoj23@gmail.com>
2022-03-26 12:01:27 -0700

    created health_check for basic integration testing

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+async fn health_check() -> HttpResponse { 
+    HttpResponse::Ok().finish()
+}
+
+// Running main app
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| { 
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            })
+            .listen(listener)?
+            .run();
+    Ok(server)
+}
+
+// Get random port to listen to for user
+pub fn create_listerner() -> TcpListener {
+    let listener =  TcpListener::bind("127.0.0.1:0")
+    .expect("Failed to bind random port");
+    return listener
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::{run, create_listerner};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+
+    // Use TcpListener to get random port for the App to use
+    let address = create_listerner();
+
+    // Bubble up the io::Error if we failed to bind the adddress
+    // otherwise call .await on our server
+    run(address)?.await
 }
