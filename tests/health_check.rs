5d1bf0b672aabc2e03803ac1d424676b09dbffd8
gotuzzoj23 <gotuzzoj23@gmail.com>
2022-03-27 18:22:38 -0700

    edited travis config file

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,22 @@
 use std::net::TcpListener;
 
+// Launch application in the background using Tokio runtime
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-bind let
+    let _ = tokio::spawn(server);
+
+    // Return the application address to the caller!
+    return format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
@@ -18,21 +35,4 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-// Launch application in the background using Tokio runtime
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-bind let
-    let _ = tokio::spawn(server);
-
-    // Return the application address to the caller!
-    return format!("http://127.0.0.1:{}", port)
 }
