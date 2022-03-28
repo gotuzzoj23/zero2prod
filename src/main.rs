a63d6e4865e5bb1dbf5948050b6afe2ad3b8ebba
gotuzzoj23 <gotuzzoj23@gmail.com>
2022-03-27 18:13:49 -0700

    edits

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::{run, create_listener};
 async fn main() -> std::io::Result<()> {
 
     // Use TcpListener to get random port for the App to use
-    let listener = create_listerner();
+    let listener = create_listener();
     // Bubble up the io::Error if we failed to bind the adddress
     // otherwise call .await on our server
     run(listener)?.await
