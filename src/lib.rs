57d5ba2cd12fbe2ddba2cceee61630d0cc1819fb
gotuzzoj23 <gotuzzoj23@gmail.com>
2022-03-27 17:59:54 -0700

    format updates

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
 }
 
 // Get random port to listen to for user
-pub fn create_listerner() -> TcpListener {
+pub fn create_listener() -> TcpListener {
     let listener =  TcpListener::bind("127.0.0.1:0")
     .expect("Failed to bind random port");
     return listener
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use zero2prod::{run, create_listerner};
+use zero2prod::{run, create_listener};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
 
     // Use TcpListener to get random port for the App to use
-    let address = create_listerner();
-
+    let listener = create_listerner();
     // Bubble up the io::Error if we failed to bind the adddress
     // otherwise call .await on our server
-    run(address)?.await
+    run(listener)?.await
 }
