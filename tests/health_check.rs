cc34c63a534fced917247d8853a85063e8c12eaf
Jose Gotuzzo <gotuzzoj23@gmail.com>
2022-05-25 22:23:33 -0700

    Postgres integration

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -45,6 +45,12 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration()
+        .expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
@@ -60,12 +66,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Assert
     assert_eq!(200, response.status().as_u16());
 
-    let configuration = get_configuration()
-        .expect("Failed to read configuration");
-    let connection_string = configuration.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to Postgres.");
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
     .fetch_one(&mut connection)
