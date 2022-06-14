f2bbb79a76c0398c2170fbdde0034b2a19070390
Jose Gotuzzo <gotuzzoj23@gmail.com>
2022-06-13 20:44:07 -0700

    replaced actix-logger with actix-tracing to support same request IDs

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,9 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 // Running main app
 pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
@@ -12,7 +12,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std
     // Capture 'connection' from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
